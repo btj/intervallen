df2096d299b6390cbc60c73be9ed91938604461a
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2023-02-20 09:52:35 +0100

    Documentatie

--- intervallen/src/intervallen/Interval.java
+++ intervallen/src/intervallen/Interval.java
@@ -1,11 +1,26 @@
 package intervallen;
 
+/**
+ * Each instance of this class represents an
+ * interval on the real line.
+ * 
+ * abstractetoestandsinvariant
+ * 
+ * @invar | getOndergrens() <= getBovengrens()
+ */
 class Interval {
+	/**
+	 * representatie-invariant
+	 * 
+	 * @invar | 0 <= lengte
+	 */
 	// representatie =
 	// voorstelling in het geheugen
 	private int ondergrens;
 	private int lengte;
 	
+	// abstracte toestandsruimte =
+	// verzameling van mogelijke abstracte toestanden
 	int getOndergrens() { // instantiemethode
 		return this.ondergrens;
 	}
@@ -13,11 +28,25 @@ class Interval {
 		return this.ondergrens + this.lengte;
 	}
 
+	/**
+	 * Initializes this instance with the given ondergrens and bovengrens.
+	 * 
+	 * @pre | ondergrens <= bovengrens
+	 * @post | getOndergrens() == ondergrens
+	 * @post | getBovengrens() == bovengrens
+	 */
 	Interval(int ondergrens, int bovengrens) {
 		this.ondergrens = ondergrens;
 		this.lengte = bovengrens - ondergrens;
 	}
 
+	/**
+	 * @pre | meting2 != null
+	 * @inspects | this, meting2
+	 * @creates | result
+	 * @post | result.getOndergrens() == getOndergrens() + meting2.getOndergrens()
+	 * @post | result.getBovengrens() == getBovengrens() + meting2.getBovengrens()
+	 */
 	Interval telOp(Interval meting2) {
 		return new Interval(
 				this.ondergrens + meting2.ondergrens,
